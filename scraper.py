798ac0c0d1ddbf1296b9c5e0d6c344eb0b6634b3
Parv Jain <parv.wutizup@gmail.com>
2015-10-08 21:13:35 +0530

    models added!

--- scraper.py
+++ scraper.py
@@ -6,12 +6,19 @@ from secret import cookie
 def main(url):
 	r = requests.get(url, cookies=cookie, verify=False)
 	tree = html.fromstring(r.content)
-	traveler = tree.xpath('//div[@id="m_group_stories_container"]')[0]
-	status = tree.xpath('//div[@id="m_group_stories_container"]//span')[0]
+	postURI = tree.xpath('.//a[text()="Full Story"]')
 	nextPage = tree.xpath('.//a/span[text()="See More Posts"]')[0]
-	print nextPage.getparent().xpath('@href')[0]
-	print(etree.tostring(nextPage, pretty_print=True))
 
+	for x in postURI:
+		print x.xpath('@href')
+
+	curUrl = nextPage.getparent().xpath('@href')[0]
+	finURL = "/groups/625191517538301?bacr=1388816159%3A633308973393222&refid=18"
+
+	if curUrl == finURL:
+		for x in postURI:
+			print x.xpath('@href')[0]
+	main("https://mbasic.facebook.com" +curUrl)
 
 if __name__ == "__main__":
 	main("https://mbasic.facebook.com/groups/625191517538301")
